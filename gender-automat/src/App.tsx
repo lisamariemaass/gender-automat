b4fdf09d70cf97dc6e882572a6fc9024b880a6dd
Lisa-Marie Maaß <lisamarie.maass@hotmail.com>
2025-02-21 20:35:52 +0100

    addedf header footer and language context to app

--- gender-automat/src/App.tsx
+++ gender-automat/src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { LanguageProvider } from './contexts/LanguageContext'; // Importiere den LanguageProvider
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LanguageProvider>
+      <div className="App">
+        <Header />
+        {/* Weitere Komponenten hier */}
+        <Footer />
+      </div>
+    </LanguageProvider>
   );
-}
+};
 
 export default App;
